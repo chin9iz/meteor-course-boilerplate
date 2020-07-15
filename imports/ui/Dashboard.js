ec749ace49fbdca34196801d3f0997d1ff46e4b9
Chingiz Hajiverdiyev <chingizhaciverdiyev98@gmail.com>
2020-07-15 12:18:04 +0400

    init commit

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import LinksList from './LinksList';
+import PrivateHeader from './PrivateHeader';
+
+export default () => {
+    return (
+        <div>
+            <PrivateHeader title='Dashboard'/>
+            <div className='page--content'>
+                Dashboard page content
+            </div>
+        </div>
+    );
+};
